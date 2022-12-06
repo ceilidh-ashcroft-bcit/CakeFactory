4543462818fcb368a2b3a42434d17454635b2cf0
Tony Kieling <tony.kieling@gmail.com>
2022-12-05 18:30:06 -0800

    starting doing shape - controller, repo, and view

---
+++ CakeFactoryProd/Controllers/ShapeController.cs
@@ -0,0 +1,34 @@
+﻿using CakeFactoryProd.Models;
+using CakeFactoryProd.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CakeFactoryProd.Controllers
+{
+    public class ShapeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var shape = new ShapeRepository();
+
+            return View(shape.GetAllShapes());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var shape = new ShapeRepository();
+
+            return View(shape.GetShapeById(id));
+        }
+
+        public IActionResult Create()
+        {
+            CakeFactoryContext db = new CakeFactoryContext();
+            //Console.WriteLine("=================: " + db.Manufacturers);
+            //ViewData["Mfg"] = new SelectList(db.Manufacturers, "Mfg", "Mfg");
+            //ViewData["Vendor"] = new SelectList(db.Suppliers, "Vendor", "Vendor");
+
+            return View();
+        }
+    }
+}
---
+++ CakeFactoryProd/Repositories/ShapeRepository.cs
@@ -0,0 +1,73 @@
+﻿using CakeFactoryProd.Models;
+
+namespace CakeFactoryProd.Repositories
+{
+    /// <summary>
+    /// Class responsable for the ShapeReposiory and all its methods to the DB.
+    /// </summary>
+    public class ShapeRepository
+    {
+        public Shape GetShapeById(int id)
+        {
+            CakeFactoryContext db = new CakeFactoryContext();
+            var shape = db.Shapes
+                            .Where(s => s.Id == id)
+                            .FirstOrDefault();
+
+            return shape;
+        }
+
+        public List<Shape> GetAllShapes()
+        {
+            CakeFactoryContext db = new CakeFactoryContext();
+
+            return db.Shapes.ToList();
+        }
+
+        public string CreateShape(Shape newShape)
+        {
+            //// add tuple and return message + new shape item
+
+            var message = "";
+            try
+            {
+                CakeFactoryContext db = new CakeFactoryContext();
+
+                db.Add(newShape);
+                db.SaveChanges();
+
+                message = "New shape has been added succesfully!";
+
+            }
+            catch (Exception error)
+            {
+                message = $"#Error ShapeC-01: {error.Message}";
+                Console.WriteLine(message);
+            }
+
+            return message;
+        }
+
+        public string UpdateShape(Shape updateShape)
+        {
+            ////// also need to return tuple - string + object
+            var message = "";
+            try
+            {
+                CakeFactoryContext db = new CakeFactoryContext();
+
+                db.Update(updateShape);
+                db.SaveChanges();
+
+                message = "The update has been done successfully.";
+            }
+            catch (Exception err)
+            {
+                message = $"#Error ShapeU-02: {err.Message}";
+            }
+
+            return message;
+        }
+
+    }
+}
