536f811a8b0be6540de7244d6ae1de2b6ddf2c28
ceilidh-ashcroft-bcit <cashcroft1@my.bcit.ca>
2023-02-10 14:07:01 -0800

    Added controller indexes for topping/filling crud pages

--- CakeFactoryProd/Controllers/ToppingsController.cs
+++ CakeFactoryProd/Controllers/ToppingsController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CakeFactoryProd.Data;
+using CakeFactoryProd.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CakeFactoryProd.Controllers
 {
     public class ToppingsController : Controller
     {
+        private readonly CakeFactoryContext _context;
+
+        public ToppingsController(CakeFactoryContext context) { _context = context; }
         public IActionResult Index()
         {
-            return View();
+            ToppingsRepo toppingsRepo = new ToppingsRepo(_context);
+            var toppings = toppingsRepo.GetAllToppings();
+            return View(toppings);
         }
     }
 }
