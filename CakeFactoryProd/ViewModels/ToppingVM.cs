b6f6617dc069bf85172d7516b1a44a0db5231696
ceilidh-ashcroft-bcit <cashcroft1@my.bcit.ca>
2023-02-10 13:04:53 -0800

    Added preliminary Toppings CRUD operations

---
+++ CakeFactoryProd/ViewModels/ToppingVM.cs
@@ -0,0 +1,11 @@
+﻿namespace CakeFactoryProd.ViewModels
+{
+    public class ToppingVM
+    {
+        public int Id { get; set; }
+        public string Flavor { get; set; } = null!;
+        public double PriceFactor { get; set; }
+        public string? Description { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
